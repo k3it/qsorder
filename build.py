7a7e736d30225ad26d8abec565b8aa59c6b95631
k3it <gokoyev@gmail.com>
2018-06-02 16:43:08 -0400

    update build scripts

--- build.py
+++ build.py
@@ -2,7 +2,7 @@ from cx_Freeze import setup, Executable
 
 # Dependencies are automatically detected, but it might need
 # fine tuning.
-buildOptions = dict(packages = [], excludes = [], include_msvcr = True, include_files = ['BUGS.txt', 'CHANGES.txt'])
+buildOptions = dict(packages = [], excludes = [], include_msvcr = True, include_files = ['BUGS.txt', 'CHANGES.txt', 'lame.exe'])
 
 base = 'Console'
 
