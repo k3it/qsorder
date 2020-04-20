4d922d68362c33659fc6bcbf5f1d42d27b78f44f
k3it <gokoyev@gmail.com>
2020-04-20 16:55:57 -0400

    update setup.py for building the .msi pkg tag as ver 3.1

--- pyQsorder/setup.py
+++ pyQsorder/setup.py
@@ -9,7 +9,14 @@ import sys
 base = 'Win32GUI' if sys.platform == 'win32' else None
 
 executables = [
-    Executable('qsorder.py', base=base, targetName='pyQsorder')
+    Executable('qsorder.py',
+               base=base,
+               targetName='pyQsorder',
+               icon='qsorder.ico',
+               shortcutName='Qsorder',
+               shortcutDir="DesktopFolder",
+               copyright='2020 K3IT',
+               )
 ]
 
 build_exe_options = {
@@ -18,11 +25,13 @@ build_exe_options = {
         'atexit',
         'pkg_resources',
         'pkg_resources.py2_warn',
-    ]
+    ],
+    'include_msvcr': True,
+    'optimize': 2
 }
 
 setup(name='Qsorder',
-      version='5.1',
+      version='3.1',
       description='Qsorder is a contest QSO recorder',
       # options = dict(build_exe = buildOptions),
       options={'build_exe': build_exe_options},
